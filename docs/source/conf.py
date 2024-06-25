541ec3189128587fff3ff590d90df302a1d0191d
Madelyn Broome <mbroome9@gmail.com>
2024-06-25 12:35:17 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Wind-AE'
+copyright = '2024, Broome et al.'
+author = ''
 
 release = '0.1'
 version = '0.1.0'
